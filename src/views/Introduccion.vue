a71252f1f6ac3f36f2e6ade32f1266133329ce21
lizmanchego <karinamanchego0416@gmail.com>
2026-09-12 11:36:41 -0500

    ADD Video YT + Podcast

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -23,7 +23,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/cEJQSM8UG3Q?si=yYXwdp_QuXAPoUWM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
